d08aa87baac3e744f4d2dea16bd2a5d3041b40ad
VanTigranyan <lostfank@gmail.com>
2018-09-13 00:48:09 +0400

    Added SignIn component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Tilt from "react-tilt";
 import brain from './brain.png';
+import './stylesheet.css';
 import './Logo.css';
 
 const Logo = () => {
@@ -9,10 +10,11 @@ const Logo = () => {
       <Tilt
         className="Tilt tilt"
         options={{ max: 55 }}
-        style={{ height: 150, width: 150 }}
+        style={{ height: 200, width: 200 }}
       >
         <div className="Tilt-inner pa3">
             <img src={brain} alt=""/>
+            <h2>SmartBrain</h2>
         </div>
       </Tilt>
     </div>
