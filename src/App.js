23468eb5bbf0802354de458ba9395b7390ea7048
VanTigranyan <lostfank@gmail.com>
2018-09-13 00:07:34 +0400

    Added face recognition API. bounding box of detected faces was added.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Clarifai from 'clarifai';
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
@@ -20,7 +21,57 @@ const particlesOptions = {
     }
   }
 };
+
+const app = new Clarifai.App({
+  apiKey: '26c8120c9056421aac50883fa812f7a0'
+});
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    };
+  }
+
+  calculateFaceLocation = ( data ) => {
+    console.log(data);
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
+  }
+
+  onInputChange = event => {
+    console.log(event.target.value);
+    this.setState({
+      input: event.target.value
+    });
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+    this.setState({
+      imageUrl: this.state.input
+    });
+
+    app.models.predict( Clarifai.FACE_DETECT_MODEL, this.state.input )
+      .then( response => this.displayFaceBox(this.calculateFaceLocation( response )) )
+      .catch( error => console.warn( 'You have an error -' + error ) );
+  }
   render() {
     return (
       <div className="App">
@@ -28,8 +79,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        <FaceRecognition />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import './FaceRecognition.css'
 
-const FaceRecognition = () => {
-    return (
-        <p></p>
-    )
-}
+const FaceRecognition = ({ imageUrl, box }) => {
+  return (
+    <div className="center ma">
+      <div className="mt3 absolute face">
+        <img id='inputImage' src={imageUrl} alt="" width='500px' height='auto' />
+        <div 
+        className='bounding-box' 
+        style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}
+        >
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default FaceRecognition;
